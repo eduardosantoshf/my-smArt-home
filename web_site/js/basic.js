aec3cd1d6e147754c7ec11f429c1f79e6b6a4942
Eduardo Santos <eduardosantoshf@gmail.com>
2021-01-18 00:40:51 +0000

    Website notifications design changes made by André

--- web_site/js/basic.js
+++ web_site/js/basic.js
@@ -63,14 +63,15 @@ function initDashboard(){
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px";
+    $("#mySidebar").css("width", "300px");
+    $("#navBarIcon").css("display", "none");
   }
   
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    $("#mySidebar").css("width", "0px");
+    $("#navBarIcon").css("display", "block");
+    $("#navBarIcon").attr("src","images/notis.png");
 }
 
 
@@ -98,8 +99,11 @@ setInterval(function(){
         type:'GET',
         success: function(data, status, xhr){
             var obj = JSON.parse(data)
+            $("#navBarIcon").attr("src","images/noti.png");
             obj.notificacoes.forEach(not => {
-                $("#mynots").html($("#mynots").html() + '<div>Device: '+not.deviceId+'; Information: '+not.value+'; Date: '+not.date+'</div>');
+                ihtml='<div class="mynotsNot"><div class="mynotsTitle">'+not.deviceId+'</div><div class="mynotsTitle">'+not.value+'</div><div class="mynotsTitle">'+not.date+'</div></div>';
+                $("#mynots").html($("#mynots").html() + ihtml);
+                
             });
             //console.log(obj)
         }
